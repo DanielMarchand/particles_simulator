c35c21b18b270577ef47977c84aee35a2700d08c
Daniel Marchand <daniel.marchand@gmail.com>
2018-10-29 13:53:39 +0100

    Daniel-driver Ali-navigator first-attempt user inp

--- hw2/src/main.cc
+++ hw2/src/main.cc
@@ -19,46 +19,43 @@ std::ofstream ofstream_from_name(std::string filename){
 
 int main(int argc, char *argv[])
 {
-  ComputeArithmetic my_arithmetic;
-  ComputePi my_pi;
-  int maxiter_inp = 1000000;
-  int freq_inp = 100000;
+  std::string series_type = argv[1];
+  std::string dumper_type = argv[2];
+  int maxiter_inp = std::stoi(argv[3]);
+  int freq_inp = std::stoi(argv[4]);
 
+  std::cout << "TODO: Use shared_unique ptr" << std::endl;
+  if ( series_type == "pi" ){
+      ComputePi my_series;
+  }else if ( series_type == "algebraic"){
+      ComputeArithmetic my_series;
+  } else {
+    std::cerr << "Invalid series_type selected" << std::endl;
+  }
 
+  std::cout << "TODO: Use shared_unique ptr" << std::endl;
+  if ( dumper_type == "print" ){
+    PrintSeries my_dumper(my_series,
+                          maxiter_inp,
+                          freq_inp);
+    std::ostream ofs;  ofs =  std::cout;
+  } else if ( dumper_type == "write" ){
+    WriteSeries my_dumper(my_series,
+                          maxiter_inp,
+                          freq_inp);
+    std::cout << "TODO: fix how write handles output" << std::endl;
+    std::string filename = "myfile.txt";
+    std::ofstream ofs;  ofs =  ofstream_from_name(filename);
+  } else {
+    std::cerr << "Invalid dumper_type selected" << std::endl;
+  }
 
-  PrintSeries my_printseries(my_arithmetic,
-                             maxiter_inp,
-                             freq_inp);
+  std::cout << "TODO: setPrecision" << std::endl;
+  //my_dumper.setPrecision(7);
+  //my_dumper.dump()
 
-  PrintSeries my_printseries_pi(my_pi,
-                                maxiter_inp,
-                                freq_inp);
-
-
-  my_printseries_pi.setPrecision(7);
-  my_printseries_pi.dump();
-
-  my_printseries.setPrecision(6);
-  my_printseries.dump();
-
-
-  WriteSeries my_writeseries_pi(my_pi,
-                                maxiter_inp,
-                                freq_inp);
-
-
-  WriteSeries my_writeseries_arithmetic(my_arithmetic,
-                                        maxiter_inp,
-                                        freq_inp);
-
-  std::string filename = "myfile_pi.txt";
-  std::ofstream ofs;  ofs =  ofstream_from_name(filename);
-  my_writeseries_pi.dump(ofs);
+  my_dumper.dump(ofs);
   ofs.close();
 
-  std::string filename2 = "myfile_arithmetic.txt";
-  std::ofstream ofs2;  ofs2 =  ofstream_from_name(filename2);
-  my_writeseries_arithmetic.dump(ofs2);
-  ofs2.close();
   return 0;
 }
