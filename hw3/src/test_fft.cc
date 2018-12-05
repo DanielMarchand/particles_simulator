5f121501fefb5056c5b145606014c32922b8e0e6
Daniel Marchand <daniel.marchand@gmail.com>
2018-12-05 11:49:12 +0100

    added (failing but correct) test for inv_fft

--- hw3/src/test_fft.cc
+++ hw3/src/test_fft.cc
@@ -9,8 +9,8 @@ TEST(FFT, transform) {
 
   Real k = 2 * M_PI / N;
   for (auto&& entry : index(m)) {
-    int i = std::get<0>(entry);
-    int j = std::get<1>(entry);
+    UInt i = std::get<0>(entry);
+    UInt j = std::get<1>(entry); //NOTE j is unused here..
     auto& val = std::get<2>(entry);
     val = cos(k * i);
   }
@@ -18,8 +18,8 @@ TEST(FFT, transform) {
   Matrix<complex> res = FFT::transform(m);
 
   for (auto&& entry : index(res)) {
-    int i = std::get<0>(entry);
-    int j = std::get<1>(entry);
+    UInt i = std::get<0>(entry);
+    UInt j = std::get<1>(entry);
     auto& val = std::get<2>(entry);
     if (std::abs(val) > 1e-10)
       std::cout << i << "," << j << " = " << val << std::endl;
@@ -35,5 +35,31 @@ TEST(FFT, transform) {
 /*****************************************************************/
 
 TEST(FFT, inverse_transform) {
+  //TODO pull general elements out into a test setup
+  UInt N = 512;
+  Matrix<complex> m(N);
+
+  Real k = 2 * M_PI / N;
+  for (auto&& entry : index(m)) {
+    UInt i = std::get<0>(entry);
+    UInt j = std::get<1>(entry); //NOTE j is unused here..
+    auto& val = std::get<2>(entry);
+    val = cos(k * i);
+  }
+
+  Matrix<complex> res = FFT::transform(m);
+  Matrix<complex> inv_res = FFT::itransform(res);
+
+  // Check that the F^-1F(m) == m
+  for (auto&& entry : index(m)) {
+    for (auto&& inv_entry : index(inv_res)) {
+      auto& val = std::get<2>(entry);
+      auto& inv_val = std::get<2>(inv_entry);
+      std::cout << "val: "  << val << std::endl;
+      std::cout << "inv_val: " << inv_val << std::endl;
+      //NOTE std::abs does not check the sign adequately
+      ASSERT_NEAR(std::abs(val), std::abs(inv_val), 1e-10);
+    }
+  }
 }
 /*****************************************************************/
