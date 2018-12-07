7b20f9d2a98ed625afa3ef06bed92dfd0b36b971
Daniel Marchand <daniel.marchand@gmail.com>
2018-12-07 15:48:59 +0100

    updated settings

--- hw3/ex_inputs/run_particles.sh
+++ hw3/ex_inputs/run_particles.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
+INPUT_FILE=radial_heat_highresolution.txt
+
+if [ ! -f $INPUT_FILE ]; then
+  echo Could not find $INPUT_FILE, consider running gen_highresolution_radial.sh
+fi
+
 if [ -d ./dumps ]; then
   rm -rf ./dumps
 fi
 mkdir dumps
 
-#particles n_timesteps radius output_file particle_type timestep
-#../src/build/particles 100 1 radial_heat_highresolution.txt material_point 0.001
-../src/build/particles 100 1 radial_heat_tinyresolution.txt material_point 0.001
+N_TIMESTEPS=100
+DUMP_FREQ=10
+DELTA_T=0.0001
+../src/build/particles $N_TIMESTEPS $DUMP_FREQ $INPUT_FILE material_point $DELTA_T
